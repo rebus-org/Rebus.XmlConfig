aaf2b634971134a2c2ecbe8786d089754fff0f9d
Mogens Heller Grabe <mookid8000@gmail.com>
2016-08-31 12:19:30 +0200

    XML!!!!11

---
+++ Rebus.XmlConfig.Tests/CanReadConfigurationFile.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Routing.TypeBased;
+using Rebus.Tests.Contracts;
+using Rebus.Transport.InMem;
+using Swindler;
+
+namespace Rebus.XmlConfig.Tests
+{
+    [TestFixture]
+    public class CanReadConfigurationFile : FixtureBase
+    {
+        [Test]
+        public void ItWorks()
+        {
+            using (AppConfig.Use("Examples/App-01.config"))
+            {
+                using (var activator = new BuiltinHandlerActivator())
+                {
+                    Configure.With(activator)
+                        .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), "bimse"))
+                        .Routing(r => r.TypeBasedRoutingFromAppConfig())
+                        .Start();
+                }
+            }
+        }
+
+        [Test]
+        public void ItWorksWithComplementaryConfigAsWell()
+        {
+            using (AppConfig.Use("Examples/App-01.config"))
+            {
+                using (var activator = new BuiltinHandlerActivator())
+                {
+                    Configure.With(activator)
+                        .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), "bimse"))
+                        .Routing(r => r.TypeBased().AddEndpointMappingsFromAppConfig())
+                        .Start();
+                }
+            }
+        }
+    }
+
+    public class SomeExistingType { }
+}
